582beca49af3367baf8387cd6e5a52aca269bc84
Kishan Somaiya <61583134+kishansomaiya@users.noreply.github.com>
2023-09-18 13:24:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
     
     <section>
       <Head>
-        <title>CRUD Ancona</title>
+        <title>CRUD App</title>
         <meta name="Lorem" content="Lorem" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
